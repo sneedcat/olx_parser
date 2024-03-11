08b914526a7707b84718c7b1f05d53e34bde164c
sneedcat <basedsneedcat@proton.me>
2024-03-11 23:58:37 +0200

    add a simple web interface to see the information

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import urllib3
 import re
 import json
 import urllib.parse
+from flask import Flask, render_template, request
+from multiprocessing import Pool
 
 @dataclass
 class Offer:
@@ -25,7 +27,6 @@ class Offer:
     location: str
     searchReason: str
 
-
 def parse(data: str) -> List[Offer]:
     list = re.findall(r'window.__PRERENDERED_STATE__= "((.|\s)*?)";\n        window.__TAURUS__', data)
     if len(list) == 0:
@@ -35,8 +36,6 @@ def parse(data: str) -> List[Offer]:
     to_be_parsed = urllib.parse.unquote(to_be_parsed)
     to_be_parsed = re.sub('"links":{"self":.*?},', "", to_be_parsed)
     data = json.loads(to_be_parsed)
-    with open('data.json', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(data, indent=4))
     offers = []
     for offer in data['listing']['listing']['ads']:
         parsed_offer = Offer(
@@ -58,26 +57,39 @@ def parse(data: str) -> List[Offer]:
             price=None,
             priceCurrency=None
         )
+        parsed_offer.description = parsed_offer.description.replace('<br />', '<br>')
+        parsed_offer.description = re.sub(r"https://(.*?)\s", r"<a href='https://\1'>https://\1</a>", parsed_offer.description)
         if not parsed_offer.exchange:
             parsed_offer.priceCurrency = offer['price']['regularPrice']['currencyCode']
             parsed_offer.price = offer['price']['regularPrice']['value']
         offers.append(parsed_offer)
     return offers
 
+def fetch_page(url, page_number):
+  request = urllib3.request("GET", f"{url}?page={page_number}")
+  data = request.data.decode('utf-8')
+  return parse(data)  # Replace with your parsing logic
+
 def get_multiple_pages(url: str, pages: int) -> List[Offer]:
-    offers = []
-    for i in range(1, pages + 1):
-        request = urllib3.request("GET", url + f'?page={i}')
-        data = request.data.decode('utf-8')
-        offers += parse(data)
-    return offers
+  offers = []
+  with Pool(processes=pages) as pool:
+    # Submit tasks for fetching each page
+    results = pool.starmap(fetch_page, [(url, page_number) for page_number in range(1, pages + 1)])
+    offers = [item for sublist in results for item in sublist]  # Flatten nested list
+  return offers
 
+app = Flask(__name__)
+
+@app.route("/")
 def main():
-    url = 'https://www.olx.ro/electronice-si-electrocasnice/laptop-calculator-gaming/console-portabile/'
-    pages = 2
-    offers = get_multiple_pages(url, pages)
-    for offer in offers:
-        print(offer)
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    main()
+@app.route("/search", methods=['POST'])
+def search():
+    url = request.form['url']
+    pages = int(request.form['pages'])
+    sponsored = request.form.get('sponsored')
+    offers = get_multiple_pages(url, pages)
+    if not sponsored:
+        offers = [offer for offer in offers if not offer.isPromoted]
+    return render_template("offers.html", offers=offers)
